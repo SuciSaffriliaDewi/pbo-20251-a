dbd19612f032b396e939a44c560c6f25a793f0d5
unknown <sucisaffrilia245@gmail.com>
2025-11-01 15:51:58 +0700

    ini pakai method

--- luasLingkaran.php
+++ luasLingkaran.php
@@ -4,10 +4,21 @@ class luasLingkaran {
 	
       public const phi = 3.14;
       public int $jari;
+
+      public function tampil($nama = 'ban') {
+            $rumus = luasLingkaran::phi * ($this->jari * $this->jari);
+            echo "Lingkaran {$nama} ini hasilnya adalah: {$rumus}";
+      }
+
+      public static function testing() {
+            echo "<br/>";
+            echo "Ini dari static";
+      }
+
 }
 
 $lingkaran = new luasLingkaran();
 $lingkaran->jari = 12;
+$lingkaran->tampil('roda'); //panggil method tampil
 
-$rumus = luasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-echo "Hasilnya adalah: ".$rumus;
+luasLingkaran::testing(); //panggil static method
