69daeb049101b620e1be03221e60eaa275a461a6
unknown <sucisaffrilia245@gmail.com>
2025-11-01 16:00:07 +0700

    ini pakai constructor

--- luasLingkaran.php
+++ luasLingkaran.php
@@ -5,6 +5,10 @@ class luasLingkaran {
       public const phi = 3.14;
       public int $jari;
 
+      public function __construct($isijari = 1) {
+            $this->jari = $isijari;
+      }
+
       public function tampil($nama = 'ban') {
             $rumus = luasLingkaran::phi * ($this->jari * $this->jari);
             echo "Lingkaran {$nama} ini hasilnya adalah: {$rumus}";
@@ -15,10 +19,15 @@ class luasLingkaran {
             echo "Ini dari static";
       }
 
+      public function __destruct() {
+            echo "<br/>";
+            echo "Udah ah cape";
+      }
+
 }
 
-$lingkaran = new luasLingkaran();
-$lingkaran->jari = 12;
+$lingkaran = new luasLingkaran(12);
+// $lingkaran->jari = 12;
 $lingkaran->tampil('roda'); //panggil method tampil
 
 luasLingkaran::testing(); //panggil static method
